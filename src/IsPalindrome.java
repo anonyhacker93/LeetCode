94d67bd97e033f474ce3f04392530bb034a1ccc8
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-05-29 16:50:19 +0530

    fix palindrome issue

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -1,20 +1,17 @@
 public class IsPalindrome {
 
     public static void main(String... a) {
-        boolean isPalindrome = IsPalindrome.isPalindrome(0);
+        boolean isPalindrome = IsPalindrome.isPalindrome(121);
         System.out.print(isPalindrome);
     }
     static boolean isPalindrome(int x) {
-        if (x >= 0) {
             int revNo = 0;
             int no = x;
             while (no > 0) {
-                no = no % 10;
-                revNo = revNo * 10 + no;
+                int rem = no % 10;
+                revNo = revNo * 10 + rem;
                 no = no / 10;
             }
-            return no == revNo;
-        }
-        return false;
+            return x == revNo;
     }
 }
